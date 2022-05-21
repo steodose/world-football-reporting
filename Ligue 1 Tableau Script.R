e75fbf5990513bec58bbce910be0bc220839977a
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2022-05-21 00:28:56 -0700

    Adding French league

--- Ligue 1 Tableau Script.R
+++ Ligue 1 Tableau Script.R
@@ -1,4 +1,4 @@
-##### 2021-22 Bundesliga Matches Script for Tableau Dashboard #####
+##### 2021-22 Ligue 1 (France) Matches Script for Tableau Dashboard #####
 ##### By: Stephan Teodosescu #####
 ##### May 2022 #####
 
@@ -13,13 +13,13 @@ library(googlesheets4)
 
 #matchday_table <- tm_matchday_table(country_name="England", start_year="2021", matchday=c(1:30))
 
-# Alternatively load 2021-22 Bundesliga Game Data from football-data.com
-bund_results <- read.csv("https://www.football-data.co.uk/mmz4281/2122/D1.csv", 
-                        stringsAsFactors = FALSE)
+# Alternatively load 2021-22 Ligue 1 Game Data from football-data.com
+fr_results <- read.csv("https://www.football-data.co.uk/mmz4281/2122/F1.csv", 
+                         stringsAsFactors = FALSE)
 
 
 # Process data frame to get one row per team-game
-bund_results2 <- bund_results %>% 
+fr_results2 <- fr_results %>% 
     select(Date:FTR) %>%
     pivot_longer(contains('Team'), names_to = 'home_away', values_to = 'team', names_prefix = 'team_') %>% 
     mutate(score = ifelse(home_away == "HomeTeam", FTHG, FTAG),
@@ -31,7 +31,7 @@ bund_results2 <- bund_results %>%
 
 
 # calculate the running counts for points, wins, and GD
-matchday_table_bund <- bund_results2 %>% 
+matchday_table_fr <- fr_results2 %>% 
     select(-c(Time, FTHG, FTAG, FTR)) %>%
     group_by(team) %>%
     mutate(goal_diff = score - opp_score) %>% 
@@ -45,12 +45,12 @@ matchday_table_bund <- bund_results2 %>%
 ##### Write to googlesheets for Tableau fun #####
 
 # Need to do this only once to initialize new googlesheet. Creates a sheet called matchday_table. The workbook name is sheets.
-#tableau_bund <- gs4_create("matches_bund", sheets = matchday_table_bund)
+#tableau_fr <- gs4_create("matches_fr", sheets = matchday_table_fr)
 
 # calling this will write to Googlesheets
-#tableau_bund
+#tableau_fr
 
 # update data (run this every week after the games are over)
-sheet_write(matchday_table_bund, ss = "https://docs.google.com/spreadsheets/d/1dy5dFMKpvwkGUxsxnbFloZx1tyd5w9QV7nh4qZn3boc/edit#gid=23630094", 
-            sheet = "matchday_table_bund")
+sheet_write(matchday_table_fr, ss = "https://docs.google.com/spreadsheets/d/1_0nwnvEaPcL2IkFfC3-AnCI0UwTr8a-fixuVijXRYJs/edit#gid=385669297", 
+            sheet = "matchday_table_fr")
 
